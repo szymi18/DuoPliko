72a1dea0173862216da8538f889b3ad6c8575007
szymon <szymek0705@gmail.com>
2024-11-12 11:10:55 +0100

    make layout.ui and layout2.ui files

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import sys
 
-def main():
+from PyQt6.QtWidgets import QDialog, QApplication
+
+from layout import Ui_Dialog
+
+
+class MyForm(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
 
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyForm()
+    form.show()
+    sys.exit(app.exec())
 
 
